307a1740796413610dcbc0f81959202905a1623d
AdelVa <radioburst@bk.ru>
2024-04-25 17:35:04 +0300

    Steps 1-2.

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { program } from "commander";
+
+program
+  .version("0.0.1", "-V, --version", "output the version number")
+  .description("Compares two configuration files and shows a difference.")
+  .help("-h, --help", "output usage information")
+  .option("-f, --format [type]", "output format", "stylish");
+
+program.parse();

b400557109a86e9dc63c5b878353843bb76ca7eb
AdelVa <radioburst@bk.ru>
2024-04-26 15:29:35 +0300

    Step 2, something is broken

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,11 @@ program
   .version("0.0.1", "-V, --version", "output the version number")
   .description("Compares two configuration files and shows a difference.")
   .help("-h, --help", "output usage information")
-  .option("-f, --format [type]", "output format", "stylish");
+  .option("-f, --format [type]", "output format", "stylish")
+  .argument("filepath1", "Path to the first configuration file.")
+  .argument("filepath2", "Path to the second configuration file.")
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
 
 program.parse();

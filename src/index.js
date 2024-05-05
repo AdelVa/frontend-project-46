3e371f7112b34464bd14d25ee634d36fc348602c
AdelVa <radioburst@bk.ru>
2024-05-05 16:28:20 +0300

    Step 3

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import { parseFile } from "./script.js";
 
 const getPath = (filepath) => path.resolve(process.cwd(), filepath);
 
-const gendiff = (filepath) => {
+const genDiff = (filepath) => {
   const file = getPath(filepath);
   const readFile = fs.readFileSync(file, { encoding: "utf-8" });
   const data = parseFile(readFile);
   return data;
 };
 
-console.log(gendiff(`../__fixtures__/file1.json`));
+export default genDiff;

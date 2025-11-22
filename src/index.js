295442dfc8a9e3e535284eb5d03bc05dc5dda3ca
AdelVa <radioburst@bk.ru>
2025-11-22 15:37:52 +0300

    Step 7: small fixes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const getExt = (filepath) => {
   return fileExt;
 };
 
-const genDiff = (filepath1, filepath2, format = 'stylish') => {
+const genDiff = (filepath1, filepath2) => {
   const file1 = getPath(filepath1);
   const file2 = getPath(filepath2);
 

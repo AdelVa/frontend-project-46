83a8a3051944d389bdc1e13dc1049ef059faf882
AdelVa <radioburst@bk.ru>
2025-12-08 13:38:27 +0300

    added test imports

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,13 +1,16 @@
 import { fileURLToPath } from 'url'
 import fs from 'fs'
 import path from 'path'
+import { expect, test } from '@jest/globals'
 import genDiff from '../src/index.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
-const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8')
+const getFixturePath = filename =>
+  path.join(__dirname, '..', '__fixtures__', filename)
+const readFile = filename =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8')
 
 test('test JSON read', () => {
   const file1 = getFixturePath('file1.json')
